120cc3c78374bb591056a53202e2a4cd380e22cc
kmstewar33 <kmstewar@andrew.cmu.edu>
2024-10-07 12:35:24 -0400

    Reset 2.1 & Started 2.2

--- solution-hw5:/shopping-cart.js
+++ solution-hw5:/shopping-cart.js
@@ -1,43 +1,3 @@
-const glazingOptions = [
-    {name: `Keep original`, glazingPrice: 0.00},
-    {name: `Sugar Milk`, glazingPrice: 0.00},
-    {name: `Vanilla Milk`, glazingPrice: 0.50},
-    {name: `Double-Chocolate`, glazingPrice: 1.50}
-]
-
-const packSize = [
-    {amount: `1`, packPrice: 1},
-    {amount: `3`, packPrice: 3},
-    {amount: `6`, packPrice: 5},
-    {amount: `12`, packPrice: 10},
-]
-
-const rolls = {
-    "Original": {
-        "basePrice": 2.49,
-        "imageFile": "original-cinnamon-roll.jpg"
-    },
-    "Apple": {
-        "basePrice": 3.49,
-        "imageFile": "apple-cinnamon-roll.jpg"
-    },
-    "Raisin": {
-        "basePrice": 2.99,
-        "imageFile": "raisin-cinnamon-roll.jpg"
-    },
-    "Walnut": {
-        "basePrice": 3.49,
-        "imageFile": "walnut-cinnamon-roll.jpg"
-    },
-    "Double-Chocolate": {
-        "basePrice": 3.99,
-        "imageFile": "double-chocolate-cinnamon-roll.jpg"
-    },
-    "Strawberry": {
-        "basePrice": 3.99,
-        "imageFile": "strawberry-cinnamon-roll.jpg"
-    }    
-};
 
 let cart = [];
 
@@ -46,56 +6,12 @@ class Roll {
         this.type = rollType; /* storing the role name into a property called type */
         this.glazing = rollGlazing; /* storing the glazing input into a property called glazing */
         this.size = packSize; /* storing the pack size input into a property called size */
-        this.totalPrice = this.calculatePrice(); /* storing the total roll price per role name into a property called base price */
+        this.totalPrice = rollPrice; /* storing the total roll price per role name into a property called base price */
 
         this.element = null; 
     }
 
-    glazingPrice(){
-        let glazePrice = 0;
-        for(i = 0; i < Roll.glazing.length; i++){
-            if (Roll.glazing === "Keep original" || Roll.glazing === "Sugar Milk") {
-                glazePrice = 0.00;
-            }
-            else if (Roll.glazing === "Vanilla Milk") {
-                glazePrice = 0.50;
-            }
-            else if (Roll.glazing === "Double-Chocolate") {
-                glazePrice = 1.50;
-            }
-        }
-        return glazePrice;
-    }
-
-    packingPrice(){
-        let packPrice = 0;
-        for(i = 0; i < Roll.size.length; i++){
-            if (Roll.size === "1") {
-                packPrice = 1;
-            }
-            if (Roll.size === "3") {
-                packPrice = 3;
-            }
-            if (Roll.size === "6") {
-                packPrice = 5;
-            }
-            if (Roll.size === "12") {
-                packPrice = 10;
-            }
-        }
-        return packPrice;
-    }
-
-    calculatePrice(){
-        let glze = glazingPrice();
-        let pck = packingPrice();
-
-        let totalPrice = (Roll[rollType].basePrice + glze) * pck;
-
-        return totalPrice;
-        
-    }
-
+    /* missing a method here to calculate rollPrice... not sure how to nest this, what other pieces of code from OG JS need to be added */
 }
 
 const rollSet = new Set();
@@ -106,21 +22,22 @@ function addNewRoll(rollType, rollGlazing, packSize, rollPrice) {
     return bunBun;
 }
 
-const rollOne = addNewRoll (
-    "Original", "Sugar Milk", "1", 
-);
+const rollOne = addNewRoll ("Original", "Sugar Milk", "1");
 
-const rollTwo = addNewRoll (
-    "Walnut", "Vanilla Milk", "12",
-);
+const rollTwo = addNewRoll ("Walnut", "Vanilla Milk", "12");
 
-const rollThree = addNewRoll (
-    "Raisin", "Sugar Milk", "3",
-);
+const rollThree = addNewRoll ("Raisin", "Sugar Milk", "3");
 
-const rollFour = addNewRoll (
-    "Apple", "Original", "3",
-);
+const rollFour = addNewRoll ("Apple", "Original", "3");
+
+for (const bunBun of rollSet) {
+    console.log(bunBun);
+    createElement(bunBun);
+} 
+
+function createElement(bunBun){
+    console.log("creating an element!");
+}
 
 
 
