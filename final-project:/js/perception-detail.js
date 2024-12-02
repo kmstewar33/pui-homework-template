5f1927f69a671dc754e82e5a4f1c01f4fb15b4f2
kmstewar33 <kmstewar@andrew.cmu.edu>
2024-12-02 07:28:07 -0500

    testing console/params

--- final-project:/js/perception-detail.js
+++ final-project:/js/perception-detail.js
@@ -1,12 +1,3 @@
-/* testing */
-
-console.log("started application");
-
-function updateElement() {
-    console.log("running the element update function!");
-}
-
-
 /* setting up params & outline for each roll */
 const queryString = window.location.search;
 console.log(queryString);
@@ -20,5 +11,5 @@ console.log(perceptionType);
 const headerElement = document.querySelector('#learning-style');
 headerElement.innerText = perceptionType + " perception";
 
-const styleIcon = document.querySelector('.style-img');
+const styleIcon = document.querySelector('#style-img');
 styleIcon.src = './Images/' + perceptionType + '.jpg';
