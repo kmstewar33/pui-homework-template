beccc3ec1dfb52fd0ca2fc7f1d920c827ef80e22
kmstewar33 <kmstewar@andrew.cmu.edu>
2024-12-10 12:32:19 -0500

    delete old js function not needed for textillate

--- final-project:/js/cover-text-animate.js
+++ final-project:/js/cover-text-animate.js
@@ -11,19 +11,3 @@ $(document).ready(function() {
     }); // This should animate the text x chatgpt, modifications to flash & bounce by me
   });
 
-// $(document).ready(function() {
-//     $('.nevada').lettering(); // This should split "Hello" into individual spans x chatgpt
-//     $('.nevada').textillate({
-//         autoStart: true,
-//         in: { effect: 'flash', shuffle: false }, // Shuffle off to preserve order
-//         out: { effect: 'bounce', sync: true },
-//         loop: true,
-//         minDisplayTime: 2000,
-//         initialDelay: 0,
-//         autoStart: true,
-//         callback: function () {
-//             console.log("Animation completed!");
-//         },
-//         selector: '.color-span' // Apply Textillate to your custom spans
-//     });
-// };
